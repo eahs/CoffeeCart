adf4a06e2416ccceb3272f63b0e32266657a5c7a
Denise Kaur <denisekaur2002@gmail.com>
2019-10-10 13:06:14 -0400

    Created Sugar Page

---
+++ Source/CoffeeCart/CoffeeCart/Views/SugarPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CoffeeCart.Views
+{
+    public partial class SugarPage : ContentPage
+    {
+        public SugarPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
