302530eeb4d825df90f85ddb7a8db966cc855ebf
Andrew B <andluibas@gmail.com>
2019-10-17 10:29:18 -0400

    Size Page

---
+++ Source/CoffeeCart/CoffeeCart/Views/SizePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CoffeeCart.Views
+{
+    public partial class SizeOfCoffeePage : ContentPage
+    {
+        public SizeOfCoffeePage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
